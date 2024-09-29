4267bbd7a94fd9f4fe24502a29fa12fcc9018eef
Justin Pyron <justinpyron@gmail.com>
2024-09-29 21:57:01 +0200

    Add arg to skip past table of contents; update for custom tokenizer

--- dataloader.py
+++ dataloader.py
@@ -10,14 +10,16 @@ class BookDataLoader:
         dir_with_books: str,
         train_fraction: float,
         tokenizer: tiktoken.core.Encoding,
-        window_size_default : int,
-        batch_size_default : int,
+        window_size_default: int,
+        batch_size_default: int,
+        num_lines_to_skip: int = 0,
     ) -> None:
         self.dir_with_books =  dir_with_books
         self.train_fraction = train_fraction
         self.tokenizer = tokenizer
         self.window_size_default = window_size_default
         self.batch_size_default = batch_size_default
+        self.num_lines_to_skip = num_lines_to_skip  # Avoid table of contents
         self.make_train_val_sets()
 
 
@@ -25,9 +27,9 @@ class BookDataLoader:
         gutenburg_start = "*** START OF THE PROJECT GUTENBERG EBOOK"
         gutenburg_end = "*** END OF THE PROJECT GUTENBERG EBOOK"
         lines = text.splitlines()
-        idx_start = next((line_num for line_num, line in enumerate(lines) if gutenburg_start in line), 0) + 1
+        idx_start = next((line_num for line_num, line in enumerate(lines) if gutenburg_start in line), -1) + 1
         idx_end = next((line_num for line_num, line in enumerate(lines) if gutenburg_end in line), len(lines))
-        lines = lines[idx_start : idx_end]
+        lines = lines[max(idx_start, self.num_lines_to_skip) : idx_end]
         lines = " ".join([line.strip() for line in lines if line != ""])
         return lines
 
@@ -38,7 +40,8 @@ class BookDataLoader:
         for book in os.listdir(self.dir_with_books):
             with open(os.path.join(self.dir_with_books, book), "r") as f:
                 text = self.process(f.read())
-            text_encoded = self.tokenizer.encode(text)
+            # text_encoded = self.tokenizer.encode(text) # tiktoken
+            text_encoded = self.tokenizer.encode(text).ids # Custom HuggingFace tokenizer
             threshold = int(self.train_fraction * len(text_encoded))
             self.train += text_encoded[:threshold]
             self.val += text_encoded[threshold:]
