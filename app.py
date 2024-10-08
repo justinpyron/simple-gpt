966b298d8cd74c3be03c5b71f26699ebdaa1c68b
Justin Pyron <justinpyron@gmail.com>
2024-10-08 21:39:05 +0200

    Improve app UI

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def highlight_text(
     text_full: str,
 ) -> str:
     text_generated = text_full[len(text_original):]
-    highlighted_text_1 = f'<span style="background-color:#f9e79f">{text_original}</span>'
-    highlighted_text_2 = f'<span style="background-color:#8e44ad">{text_generated}</span>'
+    highlighted_text_1 = f'<span style="background-color:#f9e79f; color:black">{text_original}</span>'
+    highlighted_text_2 = f'<span style="background-color:#8e44ad; color:white">{text_generated}</span>'
     return f'{highlighted_text_1} {highlighted_text_2}'
 
 
@@ -29,28 +29,32 @@ with st.expander("How it works"):
     # It was trained on a corpus of 40 books from project Gutenberg
     # I hope to improve performance by training on a larger dataset
 
-
+if "text" not in st.session_state:
+    st.session_state.text = ""
 user_input = st.text_area("Enter some text", "")
-text = user_input
+if len(st.session_state.text) == 0:
+    st.session_state.text = user_input
+
 num_tokens_to_generate = st.slider(
     "Number of tokens to generate",
     min_value=10,
-    max_value=100,
+    max_value=50,
     step=10,
-    value=10,
+    value=30,
 )
 
-if st.button("Generate"):
-    if text:
-        model_input = torch.tensor(tokenizer.encode(text).ids).unsqueeze(dim=0)
-        text = tokenizer.decode(model.generate(model_input, new_tokens=num_tokens_to_generate, device="cpu")[0].tolist())
-        highlighted_output = highlight_text(user_input, text)
-        st.markdown(f'<p style="font-size: 18px;">{highlighted_output}</p>', unsafe_allow_html=True)
-    else:
-        st.warning("Please enter some text before generating.")
-if st.button("Clear seed"):
-    st.success("Seed cleared!")
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Generate", type="primary", use_container_width=True):
+        if len(st.session_state.text) > 0:
+            model_input = torch.tensor(tokenizer.encode(st.session_state.text).ids).unsqueeze(dim=0)
+            st.session_state.text = tokenizer.decode(model.generate(model_input, new_tokens=num_tokens_to_generate, device="cpu")[0].tolist())
+        else:
+            st.warning("Enter some text first")
+with col2:
+    if st.button("Reset", use_container_width=True):
+        st.session_state.text = ""
 
-
-# TODO: Add `text` to st.cache. You want to persist this value until you hit a "Reset" button.
-# TODO: Improve highlighting of the text. How to deal with light vs dark mode? Hard to have a formatting that works well for both.
+if len(st.session_state.text) > 0:
+    highlighted_output = highlight_text(user_input, st.session_state.text)
+    st.markdown(f'<p style="font-size: 18px;">{highlighted_output}</p>', unsafe_allow_html=True)
